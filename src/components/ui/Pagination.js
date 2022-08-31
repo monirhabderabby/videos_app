0a4a2f7d1cc17af43a3fdb903af2fa2cab745f65
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-08-31 22:53:48 +0600

    added css for active pagination

--- src/components/ui/Pagination.js
+++ src/components/ui/Pagination.js
@@ -1,7 +1,8 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { paginateTo } from "../../features/pagination/paginationSlice";
 
 export default function Pagination() {
+    const page = useSelector((state) => state.pagination);
     const dispatch = useDispatch();
 
     const paginationHandler = (page) => {
@@ -11,25 +12,41 @@ export default function Pagination() {
         <section className="pt-12">
             <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 justify-end">
                 <div
-                    className="bg-blue-600 text-white px-4 py-1 rounded-full"
+                    className={` ${
+                        page.page === 1
+                            ? "bg-blue-600 text-white"
+                            : "bg-blue-100 text-blue-600"
+                    } px-4 py-1 rounded-full cursor-pointer`}
                     onClick={() => paginationHandler(1)}
                 >
                     1
                 </div>
                 <div
-                    className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full"
+                    className={` ${
+                        page.page === 2
+                            ? "bg-blue-600 text-white"
+                            : "bg-blue-100 text-blue-600"
+                    } px-4 py-1 rounded-full cursor-pointer`}
                     onClick={() => paginationHandler(2)}
                 >
                     2
                 </div>
                 <div
-                    className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full"
+                    className={` ${
+                        page.page === 3
+                            ? "bg-blue-600 text-white"
+                            : "bg-blue-100 text-blue-600"
+                    } px-4 py-1 rounded-full cursor-pointer`}
                     onClick={() => paginationHandler(3)}
                 >
                     3
                 </div>
                 <div
-                    className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full"
+                    className={` ${
+                        page.page === 4
+                            ? "bg-blue-600 text-white"
+                            : "bg-blue-100 text-blue-600"
+                    } px-4 py-1 rounded-full cursor-pointer`}
                     onClick={() => paginationHandler(4)}
                 >
                     4
